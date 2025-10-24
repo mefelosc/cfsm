87913b7145448e19ebd4a56fa283eda8d6788cdf
mefelosc <Carlosfelipe921sm@gmail.com>
2025-10-24 18:40:16 -0300

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
   import "../app.css"; // importa Tailwind + fonte
   export const prerender = true;
+  
 </script>
 
 <div class="bg-base-200">
   <div class="max-w-6xl mx-auto bg-base-100 shadow-sm">
       <slot />
+
 </div>
 </div>
