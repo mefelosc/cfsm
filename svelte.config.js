b5d2554a7b47d82bc52261a4a32883fee325530f
mefelosc <Carlosfelipe921sm@gmail.com>
2025-10-24 18:39:16 -0300

    Habilita prerender para todo o site

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,25 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: vitePreprocess(),
+
   kit: {
-    adapter: adapter(),
-    prerender: {
-      entries: ['*']
+    // Adaptador para sites estáticos
+    adapter: adapter({
+      // Pasta de saída padrão. O GitHub Actions irá usar essa pasta.
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined, // ou '404.html' se você tiver uma página 404 personalizada
+      precompress: false,
+      strict: true
+    }),
+    paths: {
+        // Se o seu site estiver em https://<username>.github.io/<repo-name>
+        // Coloque o nome do seu repositório aqui.
+        // Se for em https://<username>.github.io, deixe em branco.
+        base: process.env.NODE_ENV === 'production' ? '/cfsm' : '',
     }
   }
 };
