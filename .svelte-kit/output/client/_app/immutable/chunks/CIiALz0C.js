afe946de895c316b8a729e5b302243e742344c9a
Carlos Felipe Silva Melo <39221271+mefelosc@users.noreply.github.com>
2026-01-02 23:18:11 -0300

    Chore: Add .gitignore and remove node_modules from git tracking

--- .svelte-kit/output/client/_app/immutable/chunks/CIiALz0C.js
+++
@@ -1,1 +0,0 @@
-import{h as a}from"./BV4TzqJG.js";a();
--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.BO5tZJL0.js","_app/immutable/chunks/CWj6FrbW.js","_app/immutable/chunks/CIiALz0C.js","_app/immutable/chunks/BV4TzqJG.js"];
-export const stylesheets = ["_app/immutable/assets/app.BbvlK3cg.css"];
-export const fonts = [];
